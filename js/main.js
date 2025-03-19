4d08afe0ac39549e47d2af5c0e6e56de983a2170
nkabessa <40059891+nkabessa@users.noreply.github.com>
2025-03-19 11:45:48 +0000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -56,14 +56,14 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "May 13, 2025 15:00:00",
-	render: function(data) {
-	  var el = $(this.el);
-	  el.empty()
-	    //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
-	    .append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
-	    .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
-	    .append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
-	    .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
-	}
+    date: "May 13, 2025 12:00:00",  // Data alterada para 13 de Maio de 2025
+    render: function(data) {
+      var el = $(this.el);
+      el.empty()
+        //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
+        .append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
+        .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
+        .append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
+        .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+    }
 });
